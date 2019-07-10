1e3145b63380752895a42f6984e1aab105a4a451
jacobcalv <jacobcalvino@outlook.com>
2019-07-09 20:51:32 -0500

    I worked through all the this keyword examples and now I am working on the game

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -8,30 +8,43 @@
   Each constructor function has unique properties and methods that are defined in their block comments below:
 */
   
-/*
-  === GameObject ===
-  * createdAt
-  * name
-  * dimensions (These represent the character's size in the video game)
-  * destroy() // prototype method that returns: `${this.name} was removed from the game.`
-*/
 
-/*
-  === CharacterStats ===
-  * healthPoints
-  * takeDamage() // prototype method -> returns the string '<object name> took damage.'
-  * should inherit destroy() from GameObject's prototype
-*/
+  //=== GameObject ===
+function GameObject(attributes) {
+  this.createdAt= attributes.createdAt ;
+  this.name= 'Fin the Pixel';
+  this.dimensions= '1 x 1 px' ; 
+}
+GameObject.prototype.destroy = function() {
+  return `${this.name} was removed from the game.`
+  }
 
-/*
-  === Humanoid (Having an appearance or character resembling that of a human.) ===
-  * team
-  * weapons
-  * language
-  * greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
-  * should inherit destroy() from GameObject through CharacterStats
-  * should inherit takeDamage() from CharacterStats
-*/
+
+
+  //=== CharacterStats ===
+function CharacterStats() {
+this.healthPoints= '2/100';
+}
+GameObject.prototype.takeDamage = function() {
+  return `${this.name} took damage, ${GameObject.prototype.destroy}`
+} 
+
+
+
+  //=== Humanoid (Having an appearance or character resembling that of a human.) ===
+  function Humanoid(greet) {
+    this.team = 'CrazyDudes';
+    this.weapons = 'Sword', 'Bat';
+    this.language = 'english' ;
+    this.greet = greet;
+  }
+  
+CharacterStats.prototype.greet = function() {
+  return `${this.name} offers a greeting in ${this.language}.`
+ } // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
+//   * should inherit destroy() from GameObject through CharacterStats
+//   * should inherit takeDamage() from CharacterStats
+
  
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
@@ -41,7 +54,7 @@
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-/*
+
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -102,7 +115,7 @@
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+
 
   // Stretch task: 
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. when you are in the global scope if you use the .this keyword it will be the input you put in the console.log will be the "this"
+* 2. when an object is called, the object before the dot is the "this" keyword. 
+* 3. an object is created and returned by the constructor function that returns the object
+* 4. explict binding can force the object to use a certian object without putting it within the original object.
 *
 * write out a code example of each explanation above
 */
@@ -12,15 +12,78 @@
 // Principle 1
 
 // code example for Window Binding
+var exFunction = function() { 
+    console.log(this.test);
+}
+
+var test = 'hey';
+exFunction();
 
 // Principle 2
 
 // code example for Implicit Binding
+var ex2Func = function(){
+    console.log(this.realLastName)
+}
+
+var mySecretInfo = {
+    realName: 'Shrek',
+    realLastName: 'Swamp-Breath',
+    ex2Func: ex2Func
+}
+
+mySecretInfo.ex2Func();
+////////////////////////////////////////////
+
+const jacob = {
+    occupation: 'Walmart',
+    message: function (day) {
+        return 'It is ' + day + ' today. Have a great day at ' + this.occupation;  
+    }
+}
+
+console.log(jacob.message("Tuesday"));
 
 // Principle 3
 
 // code example for New Binding
+function NameSwap(name, taunt) {
+    this.name= name;
+    this.newName = 'mr. robot';
+    this.taunt = taunt;
+    this.speak = function () {
+        return `Sorry ${this.name} your new name is ${this.newName}. ${this.taunt}`
+    }
+}
+
+const swampBreathSwap = new NameSwap('Swamp Breath','LOL!');
+const jacobSwap = new NameSwap('Jacob','HaHa');
+console.log(swampBreathSwap.speak());
+console.log(jacobSwap.speak())
+
+
 
 // Principle 4
 
 // code example for Explicit Binding
+function goodbye() {
+    name = 'joe';
+    console.log(`Have yourself a good night ${this.name}`)
+}
+
+var theHuman = {
+    name: "mr. robot"
+}
+
+goodbye.call(theHuman);
+
+// function goodbye2() {
+//     name = 'joe';
+//     console.log(`Have yourself a good night ${this.name}`)
+// }
+
+// var theHuman = {
+//     name: "mr. robot"
+// }
+
+// goodbye2.apply(theHuman);
